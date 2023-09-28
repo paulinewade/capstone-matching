8329ce8e19f89c4d584ea99fa946dc3a5b9f423f
Sathvik K R <sathvikkote@tamu.edu>
2023-09-28 17:14:20 -0500

    Revert "#14 Added RSpec test cases for project form"
    This reverts commit f7156499ab398c994ed836b970e2ae6afa9989f7.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,11 +8,16 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
+    # if @project.save
+    #   redirect_to "/profregistration"
+    # else
+    #   redirect_to "/addProjects"
+    # end
     respond_to do |format|
       if @project.save
         format.html { redirect_to "/projects", notice: "Project was successfully created." }
       else
-        format.html { render :new, status: :unprocessable_entity}
+        format.html { render :new, status: :unprocessable_entity, notice: "Name can't be blank" }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
