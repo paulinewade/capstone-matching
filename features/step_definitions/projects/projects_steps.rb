a57b755a1b05cb7c55a3eae8278282aff94bf5ec
Sathvik K R <sathvikkote@tamu.edu>
2023-10-12 00:03:32 -0500

    Added Test Cases for view projects

---
+++ features/step_definitions/projects/projects_steps.rb
@@ -0,0 +1,23 @@
+# features/step_definitions/projects_steps.rb
+
+Given("the following projects exist:") do |table|
+  table.hashes.each do |row|
+    Project.create!(row)
+  end
+end
+
+Given("I am on the projects page") do
+  visit projects_path
+end
+
+Then("I should not see {string}") do |content|
+  expect(page).not_to have_content(content)
+end
+
+When("I select {string} from {string}") do |option, field|
+  select(option, from: field)
+end
+
+When("I click the {string} button") do |button_text|
+  click_button(button_text)
+end
