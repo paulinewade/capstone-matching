80aefea56c0f8dee052629091c61b9d7553e3d31
yash <43250823+yashpatil17@users.noreply.github.com>
2023-10-13 18:26:06 -0500

    Add functionality for the professor to add sections
    - Since professor authentication is not implemented, sections are created without being associated with a specific professor at this stage

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_135143) do
     t.datetime "updated_at", null: false
   end
 
+ActiveRecord::Schema[7.0].define(version: 2023_10_13_230220) do
   create_table "professors", force: :cascade do |t|
     t.string "email"
     t.string "semester", default: "{}"
@@ -79,6 +80,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_135143) do
     t.integer "scores_id"
     t.integer "attribute_id"
     t.float "feature_score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
