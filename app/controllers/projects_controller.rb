bcd28fdd9aec0e1956b520a9ee88f9f6efa2e97c
sathvikkote-tamu <142866638+sathvikkote@users.noreply.github.com>
2023-10-13 16:45:46 -0500

    Updated Add Projects (#85)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,18 +11,22 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @semesters = generate_semesters
     @project = Project.new
   end
 
   def create
+    @semesters = generate_semesters
     @project = Project.new(project_params)
+    puts("Semester name" , @project.Semester)
     respond_to do |format|
       if @project.save
         flash[:notice] = "Project was successfully created"
         format.html { redirect_to "/projects", notice: "Project was successfully created" }
       else
+        flash[:error] =  @project.errors.full_messages.join(', ')
         format.html { render :new, status: :unprocessable_entity}
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -30,5 +34,19 @@ class ProjectsController < ApplicationController
   private
     def project_params
       params.require(:project).permit(:Name, :Semester, :Sponsor, :Description, :Link)
+    end
+
+    def generate_semesters
+      current_year = Time.now.year
+      semesters = []
+
+      (0..2).each do |year_offset|
+        year = current_year + year_offset
+        semesters << "Spring #{year}"
+        semesters << "Summer #{year}"
+        semesters << "Fall #{year}"
+      end
+
+      semesters
     end
 end
