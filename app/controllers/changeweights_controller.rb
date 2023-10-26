4fc45b73e36b145a708f9ff3a375f9157ef5c3ac
Hunter Zacha <hunterzacha@tamu.edu>
2023-10-26 15:32:54 -0500

    changes

--- app/controllers/changeweights_controller.rb
+++ app/controllers/changeweights_controller.rb
@@ -7,7 +7,7 @@ class ChangeweightsController < ApplicationController
         feature_weights = params[:feature_weights]
 
         total = feature_weights.sum do |weight|
-            weight.gsub('%', '').to_f
+            weight.to_f
           end
     
         if total != 100
@@ -18,19 +18,17 @@ class ChangeweightsController < ApplicationController
 
         if feature_weights.present?
             feature_weights.each_with_index do |weight, index|
-                # Remove the '%' character and convert the weight to a float
-                parsed_weight = weight.gsub('%', '').to_f / 100.0
+                parsed_weight = weight.to_f / 100.0
 
-                # Find the corresponding record by index and update it
-                attribute_id = index + 1  # Assuming attribute_id starts from 1 and increments by 1
+                attribute_id = index + 1
                 score_attribute = ScoresAttribute.find_by(attribute_id: attribute_id)
 
                 if score_attribute
                     score_attribute.update(feature_weight: parsed_weight)
                 else
                     # Handle the case where no record with the given attribute_id is found.
-                    flash[:alert] = "No record found for attribute_id: #{attribute_id}."
-                    redirect_to root_path
+                    flash[:error] = "No record found for attribute_id: #{attribute_id}."
+                    redirect_to changeweights_path
                     return
                 end
             end
