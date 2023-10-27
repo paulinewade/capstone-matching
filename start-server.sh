bd3e2310cd50a2a9e1dc43435e4727180dfa78b4
Zachary Laguna <lagunazachary@gmail.com>
2023-10-27 16:26:44 +0000

    add cloud reset script

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # dev only
+
+/home/ubuntu/.rbenv/shims/bin/rails db:environment:set RAILS_ENV=development
+
+/home/ubuntu/.rbenv/shims/rails db:reset
+
 /home/ubuntu/.rbenv/shims/rails db:migrate RAILS_ENV=development
 
 /home/ubuntu/.rbenv/shims/rails server -p 3000 -b 0.0.0.0 > log-server.txt 2>&1
