ee961d33173057121d385e3dafe704a3306be026
hunterzacha <96556654+hunterzacha@users.noreply.github.com>
2023-10-27 10:49:42 -0500

    59 60 change weights - completed change weights feature using new db and added cucumber tests (#106)
    * Added Test Cases for view projects
    
    * Added the implementation for view projects
    
    * GOOGLE_OAUTH_CLIENT_ID and GOOGLE_OAUTH_CLIENT_SECRET added
    
    * .env removed
    
    * student_authentication functionality and rspec and cucumber test cases implemented
    
    * student model replaced by user model and role attribute added to user model in order to make application RBAC oriented.
    
    * Issue #71 generate the database schema with _new appended to the table name so as to not affect currently running database operations
    
    * Issue #50 add resume upload
    
    * completed admin landing page, has links to random page, need to change when implemented
    
    * finished manage professors page, still need to write tests
    
    * small change to admin landing
    
    * completed admin landing page, has links to random page, need to change when implemented
    
    * started change weights
    
    * mostly done with this page, works as intended, need to clean up UI
    
    * changes
    
    * finished tests for change weights
    
    ---------
    
    Co-authored-by: Sathvik K R <sathvikkote@tamu.edu>
    Co-authored-by: aakashdeep-sil <asil@tamu.edu>
    Co-authored-by: Zachary Laguna <lagunazachary@gmail.com>
    Co-authored-by: Shripad Kulkarni <53444848+Shripad1020@users.noreply.github.com>

---
+++ app/controllers/changeweights_controller.rb
@@ -0,0 +1,41 @@
+class ChangeweightsController < ApplicationController
+    def index
+        @score_attributes = ScoresAttribute.all
+    end
+
+    def save_weights
+        feature_weights = params[:feature_weights]
+
+        total = feature_weights.sum do |weight|
+            weight.to_f
+          end
+    
+        if total != 100
+            flash[:error] = "Weights do not add up to 100%, try again."
+            redirect_to changeweights_path
+            return
+        end
+
+        if feature_weights.present?
+            feature_weights.each_with_index do |weight, index|
+                parsed_weight = weight.to_f / 100.0
+
+                attribute_id = index + 1
+                score_attribute = ScoresAttribute.find_by(attribute_id: attribute_id)
+
+                if score_attribute
+                    score_attribute.update(feature_weight: parsed_weight)
+                else
+                    # Handle the case where no record with the given attribute_id is found.
+                    flash[:error] = "No record found for attribute_id: #{attribute_id}."
+                    redirect_to changeweights_path
+                    return
+                end
+            end
+            flash[:success] = "Feature weights updated successfully."
+        else
+            flash[:error] = "No feature weights provided for update."
+        end
+        redirect_to changeweights_path
+    end
+end
---
+++ features/step_definitions/change_weights_steps.rb
@@ -0,0 +1,40 @@
+When('I visit the change weights page') do
+    visit '/changeweights'
+end
+
+Given('the following score attributes exist:') do |table|
+    table.hashes.each do |score_attribute_params|
+        feature_name = score_attribute_params['feature_name']
+        feature_weight = score_attribute_params['feature_weight']
+        attribute_id = score_attribute_params['attribute_id']
+        ScoresAttribute.create!(attribute_id: attribute_id, feature_name: feature_name, feature_weight: feature_weight) 
+    end
+end
+
+Then('I should see percentage input fields') do
+    within('table') do
+      expect(page).to have_css('input[type="text"][name^="feature_weights"]')
+    end
+  end
+  
+Then('I should see a {string} button') do |button_text|
+    expect(page).to have_button(button_text)
+end
+
+
+  Then('I should see the following pre-filled feature weights') do |table|
+    expected_weights = table.raw.map { |row| row[0].to_f} # Convert from percentage to decimal
+    actual_weights = all('input[type="text"][name="feature_weights[]"]').map { |input| input.value.to_f / 100.0 } # Convert from percentage to decimal
+    expect(actual_weights).to eq(expected_weights)
+  end
+  
+  When('I fill in {string} for {string} at index {int}') do |value, feature_name, index|
+    input_fields = all(:css, "input[name='feature_weights[]']")
+    input_field = input_fields[index - 1]
+    input_field.set(value)
+  end
+  
+  Then("I should see {string} message") do |message|
+    expect(page).to have_content(message)
+  end
+  
