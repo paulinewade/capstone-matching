a6ad1677ba18eaa9a20174dd6fb03cfcc3c65bd4
Hunter_Zacha <hunterz7890@gmail.com>
2023-11-07 10:45:40 -0600

    removed old seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,41 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # db/seeds.rb
-
-ProjectNew.create(
-  project_id: 1,
-  project_name: 'Project 1',
-  description: 'We are looking for a software engineer with expertise in Ruby and Ruby on Rails.',
-  sponsor: 'Sponsor 1',
-  class_id: 1
-)
-
-ProjectNew.create(
-  project_id: 2,
-  project_name: 'Project 2',
-  description: 'Seeking a front-end developer experienced in JavaScript and React.',
-  sponsor: 'Sponsor 2',
-  class_id: 2
-)
-
-ProjectNew.create(
-  project_id: 3,
-  project_name: 'Project 3',
-  description: 'We need a data scientist skilled in Python and machine learning.',
-  sponsor: 'Sponsor 3',
-  class_id: 1
-)
-
-ProjectNew.create(
-  project_id: 4,
-  project_name: 'Project 4',
-  description: 'Seeking a back-end engineer equipped with knowledge in JavaScript and React.',
-  sponsor: 'Sponsor 4',
-  class_id: 2
-)
-
-# db/seeds/scores_attributes_seeds.rb
-
 # Seed data for the "scores_attributes" table
 form_open_datetime = DateTime.new(2023, 11, 6, 9, 0) # Example date and time
 form_close_datetime = DateTime.new(2023, 11, 6, 17, 0) # Example date and time
