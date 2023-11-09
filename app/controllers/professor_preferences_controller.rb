7cfb4116caa101b0215b3be58f386dd68ed893ac
yash <43250823+yashpatil17@users.noreply.github.com>
2023-11-09 13:34:01 -0600

    Add logic for saving professor preferences

--- app/controllers/professor_preferences_controller.rb
+++ app/controllers/professor_preferences_controller.rb
@@ -17,9 +17,29 @@ class ProfessorPreferencesController < ApplicationController
     #   current_professor.preferred_projects.destroy(project)
     #   redirect_to professor_preferences_path
     # end
-    def add_preferred
-        # puts(current_user)
-        project = Project.find(params[:id])
+    #TODO: Check if it works with OAuth and when current_user is set
+    def index
+        # puts "Curr user inspect: #{current_user.inspect}"
+        # if current_user && current_user.role == "professor"
+        # puts params
+        @projects = Project.all
+        # puts "current user is #{current_user}"
+        # puts current_user.inspect
+        # @curr_professor = User.find_by(email: current_user.email)
+        @curr_professor = User.find_by(email: "profemail@tamu.edu")
+        # puts "curr professor #{@curr_professor.inspect}"
+
+        # @curr_prof_id = @curr_professor.user_id
+        # puts "curr professor id is #{@curr_prof_id}"
+
+        @professor = @curr_professor.professor  # Get the associated Professor instance
+        @preferences = @professor.professor_preferences.includes(:project)
+
+        # @preferences = ProfessorPreference.find_by()
+        # @professor_preferences = @professor.professor_preferences.where(project_id: @projects.pluck(:id))
+        # @curr_professor_id = Professor.find_by(email: current_user.email)
+        # end
+        # @professor_preference = ProfessorPreference.find_by(email: current_user.email)
         # puts "Current User Attributes and Values:"
         # current_user.attributes.each do |attr, value|
         #     puts "#{attr}: #{value}"
@@ -28,18 +48,38 @@ class ProfessorPreferencesController < ApplicationController
         # puts "Current User Attributes and Values:"
         # puts current_user.inspect
         
-        if current_user && current_user.role == "professor"
-            # puts("i am a professor")
-            if current_user.preferred_projects.exclude?(project)
-                current_user.preferred_projects << project
-                redirect_to projects_path, notice: "Project added as preferred"
-              else
-                redirect_to projects_path, alert: "Project is already in your preferred projects"
+        # if current_user && current_user.role == "professor"
+        #     # puts("i am a professor")
+        #     if current_user.preferred_projects.exclude?(project)
+        #         current_user.preferred_projects << project
+        #         redirect_to projects_path, notice: "Project added as preferred"
+        #       else
+        #         redirect_to projects_path, alert: "Project is already in your preferred projects"
+        #     end
+        # else
+        #     # puts("i am not professor")
+        #     redirect_to projects_path, alert: "Only professors can mark projects as preferred"
+        # end
+    end
+
+    def save_rankings
+        @curr_professor = User.find_by(email: "profemail@tamu.edu")
+        # puts "curr professor #{@curr_professor.inspect}"
+
+        @professor = @curr_professor.professor  # Get the associated Professor instance
+        preferences_params = params[:preferences]
+        # puts "professor in save #{@professor}"
+
+        preferences_params.each do |project_id, pref|
+            preference = @professor.professor_preferences.find_by(project_id: project_id)
+            if preference
+              preference.update(pref: pref)
+            else
+              @professor.professor_preferences.create(project_id: project_id, pref: pref)
             end
-        else
-            # puts("i am not professor")
-            redirect_to projects_path, alert: "Only professors can mark projects as preferred"
         end
+
+        redirect_to profLanding_path, notice: 'Project preferences saved successfully.'
     end
   end
   
