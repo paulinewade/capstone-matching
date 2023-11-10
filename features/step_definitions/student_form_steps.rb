ef3139e4f7cdfad8cb2f7983b6e879594f5b7e83
Shripad Kulkarni <53444848+Shripad1020@users.noreply.github.com>
2023-11-10 17:43:12 -0600

    Studentform refactor schema and update (#132)
    * Final Schema - redone database, changes to UI to reflect scope agreement, added new models (#119)
    
    * add cloud reset script
    
    * fixed error, added nav buttons to all pages (#112)
    
    * migration to drop old tables and add new tables representing actual schema
    
    * has issues with foreign key references
    
    * can't get foreign keys to work, fix later
    
    * began fixing some parts
    
    * final migration
    
    * finalized schema migration - need to go over changes with team and seed still
    
    * started models
    
    * completed models
    
    * models complete, added deletion actions when fks are deleted
    
    * added seeds, made adjustments to models/DB to match rails functionality
    
    * updated models to no longer require validations for belongs to (or have optional) and completed seeds
    
    * small change to index, made [student,project] a unique combo
    
    * remove drop all tables migration
    
    * added form open form close to config
    
    * added index back to prof registration
    
    * changes to proflanding and adminlanding UI to reflect actual functionality
    
    ---------
    
    Co-authored-by: Zachary Laguna <lagunazachary@gmail.com>
    
    * remove extra migration
    
    * removed old seeds
    
    * remove un-needed model
    
    * refactor to new database
    
    * Hunter's refactor + new feature (#122)
    
    * fixed pages to work with new schema
    
    * started manage students page
    
    * finished manage students page - need to add tests
    
    * finished manage students page and added filter
    
    * started the add course during prof registration
    
    * done - still need to fix tests
    
    * fixed issue with flash
    
    * changed role when role is changed in table
    
    * fixed tests after refactor
    
    * Fixed oauth, still need URL to work on deployed (#125)
    
    * fixed pages to work with new schema
    
    * started manage students page
    
    * finished manage students page - need to add tests
    
    * finished manage students page and added filter
    
    * started the add course during prof registration
    
    * done - still need to fix tests
    
    * fixed issue with flash
    
    * changed role when role is changed in table
    
    * fixed oauth to work with new schema - not tested
    
    * fixed student logout
    
    * Added Sponsor Restrictions and Preferences (#123)
    
    * Add functionality to let the professor rank their preferred projects (#126)
    
    * Add project preference form for professors
    
    * Add logic for saving professor preferences
    
    * Change routes for project preferences form page
    
    * Add code to display notification in profLanding page
    
    * Change link of Project Preference button on profLanding page
    
    * Use session user_id instead of current_user
    
    * #72 test cases (#124)
    
    * Added Sponsor Restrictions and Preferences
    
    * Added Test Cases
    
    * Sorted the button size
    
    ---------
    
    Co-authored-by: hunterzacha <96556654+hunterzacha@users.noreply.github.com>
    
    * Refactor form lock (#127)
    
    * code refactored to open_close_student_form
    
    * tests added for adminlanding controller
    
    * Student form updated, schema refactoring, bug fixs and work auth field added
    
    * Fixed cucumber tests
    
    ---------
    
    Co-authored-by: hunterzacha <96556654+hunterzacha@users.noreply.github.com>
    Co-authored-by: Zachary Laguna <lagunazachary@gmail.com>
    Co-authored-by: Hunter_Zacha <hunterz7890@gmail.com>
    Co-authored-by: sathvikkote-tamu <142866638+sathvikkote@users.noreply.github.com>
    Co-authored-by: Yash Patil <43250823+yashpatil17@users.noreply.github.com>
    Co-authored-by: asil-tamu <143029155+asil-tamu@users.noreply.github.com>

--- features/step_definitions/student_form_steps.rb
+++ features/step_definitions/student_form_steps.rb
@@ -1,12 +1,15 @@
 When("I visit the StudentForm page") do
-  visit '/StudentForm'
+  visit '/studentform'
 end
 
 Given(/^a student with email "(.*?)" exists$/) do |email|
-  Professor.create(email: email, first_name: "Test", last_name: "User")
+  User.create(email: email, first_name: "Test", last_name: "User")
 end
 
 Given("I am on the student registration page") do
-  visit '/StudentForm'
+  visit '/studentform'
 end
 
+Then /^I should see "Registration Successful!" on the student registration page$/ do
+  expect(page).to have_content("Registration Successful!")
+end
