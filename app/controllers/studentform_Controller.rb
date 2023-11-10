63ffbab04a670f31019f778088d3fddba4344af3
Shripad Kulkarni <shripad1020@gmail.com>
2023-11-10 16:48:53 -0600

    Student form updated, schema refactoring, bug fixs and work auth field added

--- app/controllers/studentform_Controller.rb
+++ app/controllers/studentform_Controller.rb
@@ -1,27 +1,54 @@
 class StudentformController < ApplicationController
-    def index
-    end
+  skip_before_action :verify_authenticity_token
 
-    def create
-      # Extract form parameters
-      email = params[:email]
-      first_name = params[:first_name]
-      last_name = params[:last_name]
-      uin = params[:uin]
-      if email.end_with?("tamu.edu")
-        existing_student = StudentForm.find_by(email: email)
+  def index
+    @courses = Course.select(:id, :course_number, :section, :semester).distinct
+  end
 
-        if existing_student
-          flash[:error] = "You are already registered using this email."
-        else
-          new_student = StudentForm.new(email: email, first_name: first_name, last_name: last_name, uin: uin)
-          if new_student.save
-            flash[:success] = "Registration Successful!."
-          end
-        end
+  def create
+    # Extract form parameters
+    email = params[:email]
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    uin = params[:uin]
+    gender = params[:gender]
+
+    if email.end_with?("tamu.edu")
+      existing_student = User.find_by(email: email)
+
+      if existing_student
+        flash[:error] = "You are already registered using this email."
       else
-        flash[:error] = "Not a valid tamu.edu email address."
+        # Create a new student
+        new_student = User.new(email: email, first_name: first_name, last_name: last_name, role: "student")
+
+        # Save the student to get an ID and use that ID for the student record
+        if new_student.save
+          # Create a student record associated with the user
+          id = new_student.user_id
+          student = Student.new(
+            student_id: id,
+            course_id: 1, # you need to extract course_id from the form parameters
+            gender: gender,
+            nationality: params[:nationality], # extract other parameters as needed
+            work_auth: params[:work_authorization],
+            contract_sign: "No",
+            resume: "After resume parsing"
+          )
+
+          if student.save
+            flash[:success] = "Registration Successful!"
+          else
+            flash[:error] = "Failed to save student information."
+          end
+        else
+          flash[:error] = "Failed to save user information."
+        end
       end
-      redirect_to "/StudentForm" and return
+    else
+      flash[:error] = "Not a valid tamu.edu email address."
     end
+
+    redirect_to "/studentform"
+  end
 end
