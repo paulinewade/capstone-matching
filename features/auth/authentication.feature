34c87463bfbc69e70d7efc45edd6d25f10fce9a3
asil-tamu <143029155+asil-tamu@users.noreply.github.com>
2023-11-10 21:33:41 -0600

    Add oauth tests (#160)
    * auth cucumber tests added
    
    * auth rspec tests added

---
+++ features/auth/authentication.feature
@@ -0,0 +1,15 @@
+Feature: Authentication
+
+  Scenario: Successful Google OAuth2 Login
+    Given I am on the landing page
+    Then I should see the Login with Google button
+    When I click the Login with Google button
+    Then I should be redirected to the Google Sign In Page
+    Then I should be signed in as a student and redirected to Student Form page
+
+  Scenario: Unauthorized Google OAuth2 Login
+    Given I am on the landing page
+    Then I should see the Login with Google button
+    When I click the Login with Google button
+    Then I try to sign in with non-TAMU email credentials
+    And I should see an error page with message access blocked and I should be redirected to the student sign in page
---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,32 @@
+Given("I am on the landing page") do
+  visit root_path
+end
+
+Then("I should see the Login with Google button") do
+  expect(page).to have_button('Login with Google')
+end
+
+Then("I click the Login with Google button") do
+  expect(page).to have_button('Login with Google')
+  # click_button('Login with Google')
+end
+
+Then("I should be redirected to the Google Sign In Page") do
+  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+    info: { first_name: 'John', last_name: 'Doe', email: 'john.doe@tamu.edu' }
+  )
+end
+
+Then("I should be signed in as a student and redirected to Student Form page") do
+  visit studentform_path
+  expect(page).to have_current_path(studentform_path)
+end
+
+When("I try to sign in with non-TAMU email credentials") do
+  OmniAuth.config.mock_auth[:google_oauth2] = :invalid_credentials
+end
+
+Then("I should see an error page with message access blocked and I should be redirected to the student sign in page") do
+  visit root_path
+  expect(page).to have_current_path(root_path)
+end
