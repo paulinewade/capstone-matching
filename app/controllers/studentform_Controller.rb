04ee99c8185ef1805f5a9a767f4b7d1179809226
Hunter_Zacha <hunterz7890@gmail.com>
2023-11-14 19:41:48 -0600

    scores except resume calced and added to DB

--- app/controllers/studentform_Controller.rb
+++ app/controllers/studentform_Controller.rb
@@ -46,7 +46,11 @@ class StudentformController < ApplicationController
     @max_number = config.max_number
     @form_open = config.form_open
     @form_close = config.form_close
+    @scores_entities = ScoresEntity.where(project_id: @projects.pluck(:project_id), student_id: user_id)
     @project_ranks = {}
+    @scores_entities.each do |scores_entity|
+      @project_ranks[scores_entity.project_id] = scores_entity.pref
+    end
   end
 
   def create
@@ -70,11 +74,11 @@ class StudentformController < ApplicationController
     @form_close = config.form_close
 
     # #form isn't open
-    # if DateTime.now < @form_open || DateTime.now > @form_close
-    #   flash[:error] = "Form is not currently open, please submit during the specified window."
-    #   redirect_to studentform_path
-    #   return
-    # end
+    if DateTime.now < @form_open || DateTime.now > @form_close
+      flash[:error] = "Form is not currently open, please submit during the specified window."
+      redirect_to studentform_path
+      return
+    end
 
     if uin.to_s.length != 9
       flash[:error] = "Invalid UIN, make sure your UIN is 9 digits."
@@ -90,18 +94,21 @@ class StudentformController < ApplicationController
     end
 
     non_blank_ranks = project_ranks.to_unsafe_h.reject { |_, value| value.blank? }
+    #they rank between min and max projects
     unless non_blank_ranks.size.between?(@min_number, @max_number)
       flash[:error] = "Must rank between #{@min_number} and #{@max_number} (inclusive) projects."
       redirect_to studentform_path
       return
     end
 
+    #make sure that they dont have 2 of the same rank
     if non_blank_ranks.values.group_by { |rank| rank.to_i }.any? { |_, ranks| ranks.length > 1 }
       flash[:error] = "Duplicate ranks found for different projects. Please ensure each project has a unique rank."
       redirect_to studentform_path
       return
     end
 
+    #make sure that the ranks they don't skip any values
     if (1..non_blank_ranks.size).to_a != non_blank_ranks.values.map(&:to_i).sort
       flash[:error] = "Invalid rank sequence. Please ensure ranks are consecutive without skipping any numbers."
       redirect_to studentform_path
@@ -147,9 +154,44 @@ class StudentformController < ApplicationController
 
     if student.save
       student_id = student.student_id
+      #add_multiple ethncities
       ethnicities.each do |ethnicity|
         EthnicityValue.create(student_id: student_id, ethnicity_name: ethnicity)
       end
+
+      #add additional attributes later if needed
+      resume_attribute = ScoresAttribute.where("feature LIKE ?", "%Resume%").first
+      time_stamp_attribute = ScoresAttribute.where("feature LIKE ?", "%Time%").first
+      pref_attribute = ScoresAttribute.where("feature LIKE ?", "%Preference%").first
+
+      resume_attribute_id = resume_attribute.attribute_id if resume_attribute.present?
+      time_stamp_attribute_id = time_stamp_attribute.attribute_id if time_stamp_attribute.present?
+      pref_attribute_id = pref_attribute.attribute_id if pref_attribute.present?
+
+
+      #calculate timestamp score based on time submitted relative to the open and close time of the form
+      time_range = (@form_close - @form_open).to_f
+      time_elapsed = DateTime.now.to_f - @form_open.to_f
+      time_score = 1 - time_elapsed/time_range
+      rounded_time_score = time_score.round(2) * 100
+      pref_decrease = 1.0/@max_number.to_f
+
+      non_blank_ranks.each do |project_id, pref|
+        entity = ScoresEntity.create(student_id: student_id, project_id: project_id, pref: pref)
+        scores_id = entity.scores_id
+
+        #pref_score is 1 - ranking*(1/max_number) - we want rank 1 to be a full score which is why we subtract 1
+        pref_score = (1.0 - (pref.to_i-1)*pref_decrease) * 100
+        
+        #caluclate resume score (call helper method to compare student resume to project description here)
+        resume_score = 100
+
+        #add each score to DB under the scores_entity that it belongs to (student project pairing)
+        ScoresValue.create(scores_id: scores_id, attribute_id: time_stamp_attribute_id, feature_score: rounded_time_score)
+        ScoresValue.create(scores_id: scores_id, attribute_id: pref_attribute_id, feature_score: pref_score)
+        ScoresValue.create(scores_id: scores_id, attribute_id: resume_attribute_id, feature_score: resume_score)
+      end
+
       flash[:success] = "Registration Successful!"
     else
       flash[:error] = "Failed to save student information."
