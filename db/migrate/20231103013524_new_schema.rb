e0024d7a79c6013eceb97411ca8bd031bab67fd4
Hunter Zacha <hunterzacha@tamu.edu>
2023-11-15 16:05:34 -0600

    changed autoincrement to serial for postgres

--- db/migrate/20231103013524_new_schema.rb
+++ db/migrate/20231103013524_new_schema.rb
@@ -1,7 +1,7 @@
 class NewSchema < ActiveRecord::Migration[7.0]
   def change
     create_table :configs, id: false, primary_key: :config_id do |t|
-      t.integer :config_id, null: false, auto_increment: true, primary_key: true
+      t.serial :config_id, null: false, primary_key: true
       t.integer :min_number, default: 0, null: false
       t.integer :max_number, default: 1,  null: false
       t.datetime :form_open, null: false
@@ -9,7 +9,7 @@ class NewSchema < ActiveRecord::Migration[7.0]
     end
 
     create_table :users, id: false, primary_key: :user_id do |t|
-      t.integer :user_id, null: false, auto_increment: true, primary_key: true
+      t.serial :user_id, null: false, primary_key: true
       t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :role, null: false
@@ -24,7 +24,7 @@ class NewSchema < ActiveRecord::Migration[7.0]
     add_foreign_key :professors, :users, column: :professor_id, primary_key: :user_id, on_delete: :cascade
 
     create_table :courses, id: false, primary_key: :course_id  do |t|
-      t.integer :course_id, null: false, auto_increment: true, primary_key: true
+      t.serial :course_id, null: false, primary_key: true
       t.integer :course_number, null: false
       t.integer :section, null: false
       t.string :semester, null: false
@@ -53,7 +53,7 @@ class NewSchema < ActiveRecord::Migration[7.0]
 
     #EAV so students cant have multiple ethnicities listed
     create_table :ethnicity_values, id: false, primary_key: :ethnicity_value_id do |t|
-      t.integer :ethnicity_value_id, null: false, auto_increment: true, primary_key: true
+      t.serial :ethnicity_value_id, null: false, primary_key: true
       t.integer :student_id, null: false
       t.string :ethnicity_name, null: false
     end
@@ -62,7 +62,7 @@ class NewSchema < ActiveRecord::Migration[7.0]
     add_foreign_key :ethnicity_values, :ethnicities, column: :ethnicity_name, primary_key: :ethnicity_name, on_delete: :cascade
 
     create_table :projects, id: false, primary_key: :project_id do |t|
-      t.integer :project_id, null: false, auto_increment: true, primary_key: true
+      t.serial :project_id, null: false, primary_key: true
       t.string :name, null: false
       t.string :description, null: false
       t.string :sponsor, null: false
@@ -73,7 +73,7 @@ class NewSchema < ActiveRecord::Migration[7.0]
     add_foreign_key :projects, :courses, column: :course_id, primary_key: :course_id, on_delete: :nullify
     
     create_table :professor_preferences,id: false, primary_key: :professor_preference_id do |t|
-      t.integer :professor_preference_id, null: false, auto_increment: true, primary_key: true
+      t.serial :professor_preference_id, null: false, primary_key: true
       t.integer :professor_id, null: false
       t.integer :project_id, null: false
       t.integer :pref, null: false
@@ -84,7 +84,7 @@ class NewSchema < ActiveRecord::Migration[7.0]
 
 
     create_table :scores_entities, id: false, primary_key: :scores_id do |t|
-      t.integer :scores_id, null: false, auto_increment: true, primary_key: true
+      t.serial :scores_id, null: false, primary_key: true
       t.integer :student_id, null: false
       t.integer :project_id, null: false
       t.integer :pref, null: false #could keep null to demonstrate they didnt mark it??
@@ -95,14 +95,14 @@ class NewSchema < ActiveRecord::Migration[7.0]
     add_foreign_key :scores_entities, :projects, column: :project_id, primary_key: :project_id, on_delete: :cascade
     
     create_table :scores_attributes, id: false, primary_key: :attribute_id do |t|
-      t.integer :attribute_id, null: false, auto_increment: true, primary_key: true
+      t.serial :attribute_id, null: false, primary_key: true
       t.string :feature, null: false
       t.float :feature_weight, null: false, default: 0.0
     end
 
     #EAV between scores_entity and scores_attribute - each scores entity can have multiple scores attributes
     create_table :scores_values, id: false, primary_key: :scores_value_id do |t|
-      t.integer :scores_value_id, null: false, auto_increment: true, primary_key: true
+      t.serial :scores_value_id, null: false, primary_key: true
       t.integer :scores_id, null: false
       t.integer :attribute_id, null: false
       t.float :feature_score, null: false, default: 0.0
@@ -112,7 +112,7 @@ class NewSchema < ActiveRecord::Migration[7.0]
     add_foreign_key :scores_values, :scores_entities, column: :scores_id, primary_key: :scores_id, on_delete: :cascade
 
     create_table :sponsor_restrictions, id: false, primary_key: :restriction_id do |t|
-      t.integer :restriction_id, null: false, auto_increment: true, primary_key: true
+      t.serial :restriction_id, null: false, primary_key: true
       t.string :restriction_type, null: false #will be one of the columns on student table
       t.string :restriction_val, null: false #select values that cannot appear, if student has one of these values then they will not be allowed on the project
       t.integer :project_id, null: false
@@ -120,7 +120,7 @@ class NewSchema < ActiveRecord::Migration[7.0]
     add_foreign_key :sponsor_restrictions, :projects, column: :project_id, primary_key: :project_id, on_delete: :cascade
 
     create_table :sponsor_preferences, id: false, primary_key: :preference_id do |t|
-      t.integer :preference_id, null: false, auto_increment: true, primary_key: true
+      t.serial :preference_id, null: false, primary_key: true
       t.string :preference_type, null: false  #will be one of the columns on student table
       t.string :preference_val, null: false #values that they prefer - ie US citizen for work authorization, all other types will not be allowed on project
       t.integer :project_id, null: false
