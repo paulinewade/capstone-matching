1d8622a858f5667a823290ce1962e25cc398e0c8
Hunter_Zacha <hunterz7890@gmail.com>
2023-11-17 09:51:31 -0600

    added config so that certain pages can load

---
+++ features/support/hooks.rb
@@ -0,0 +1,6 @@
+Before do
+    # This block will run before each scenario
+    # Seed your database with the necessary data here
+    # For example, you can use FactoryBot to create records
+    FactoryBot.create(:config)
+  end
