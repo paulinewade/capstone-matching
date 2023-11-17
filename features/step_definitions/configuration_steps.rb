7b575f2a488a9d07d039f08cc0837d6783a23214
asil-tamu <143029155+asil-tamu@users.noreply.github.com>
2023-11-17 04:00:43 -0600

    Add min max (#165)
    * configuration functionality and tests added
    
    * fixed UI to use partials for consistency
    
    ---------
    
    Co-authored-by: Hunter_Zacha <hunterz7890@gmail.com>

--- features/step_definitions/configuration_steps.rb
+++ features/step_definitions/configuration_steps.rb
@@ -16,10 +16,27 @@ When("the admin clicks the {string} link") do |button_name|
 end
 
 Then("the admin should be on the {string} page") do |page_name|
-  visit open_close_student_form_path
-  expect(page).to have_current_path(open_close_student_form_path)
+  visit configuration_path
+  expect(page).to have_current_path(configuration_path)
 end
 
 Then("the admin should see {string} title") do |title|
   expect(page).to have_content(title)
+end
+
+Given(/^I am on the configuration page$/) do
+  visit configuration_path
+end
+
+When(/^I update the configuration$/) do
+  @config = Config.first_or_initialize
+  fill_in 'form_open', with: '2022-01-01 00:00:00'
+  fill_in 'form_close', with: '2022-12-31 23:59:59'
+  fill_in 'min_number', with: '1'
+  fill_in 'max_number', with: '10'
+  click_button 'Save'
+end
+
+Then(/^I should see a success message$/) do
+  expect(page).to have_content 'Changes made successfully'
 end
