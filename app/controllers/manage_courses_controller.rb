91019a132cc00159070dfcbcf3a652a75065698c
Shripad Kulkarni <shripad1020@gmail.com>
2023-11-17 04:26:52 -0600

    Displayed Course Table contained in table form

--- app/controllers/manage_courses_controller.rb
+++ app/controllers/manage_courses_controller.rb
@@ -1,12 +1,15 @@
 class ManageCoursesController < ApplicationController
 	def index
-		@courses = Course.pluck(:Semester).uniq
+		@course = Course.pluck(:Semester).uniq
 		@selected_semester = params[:semester]
 
-		@course = if @selected_semester.present?
-			Course.where(Semester: @selected_semester)
+		@courses = if @selected_semester.present?
+			Course.where(semester: @selected_semester)
 		else
 			Course.all
 		end
+	end
+
+	def edit_courses
 	end
 end
