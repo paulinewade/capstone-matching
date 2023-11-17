4e4ea0f5744600de8471c4baca440496f9c42ba1
Zachary Laguna <lagunazachary@gmail.com>
2023-11-17 16:36:25 +0000

    integrate resume match into studentform

--- app/controllers/studentform_Controller.rb
+++ app/controllers/studentform_Controller.rb
@@ -1,14 +1,24 @@
+require 'pdf/reader'
+require 'tf-idf-similarity'
+require 'matrix'
+
+
 class StudentformController < ApplicationController
   def index
-    user_id = session[:user_id]
+    # user_id = session[:user_id]
+    user_id = 101
     user = User.find_by(user_id: user_id)
     if user
-      @first_name = user.first_name
-      @last_name = user.last_name
-      @email = user.email
+      # @first_name = user.first_name UNCOMMENT ME
+      # @last_name = user.last_name
+      # @email = user.email
+      
+      @email = 'studentemail@tamu.edu'
+      @first_name = 'student'
+      @last_name = 'user'
     else
       flash[:error] = "Please login with Google account to fill out the form."
-      redirect_to root_path
+      # redirect_to root_path # UNCOMMENT ME
     end
 
     student = Student.find_by(student_id: user_id)
@@ -56,9 +66,12 @@ class StudentformController < ApplicationController
 
   def create
     # Extract form parameters
-    email = params[:email]
-    first_name = params[:first_name]
-    last_name = params[:last_name]
+    # email = params[:email] UNCOMMENT ME AND REMOVE BELOW
+    # first_name = params[:first_name]
+    # last_name = params[:last_name]
+    email = 'studentemail@tamu.edu'
+    first_name = 'student'
+    last_name = 'user'
     uin = params[:uin]
     gender = params[:gender]
     course_id = params[:course_id]
@@ -67,7 +80,8 @@ class StudentformController < ApplicationController
     nationality = params[:nationality]
     ethnicities = params[:ethnicity]
     project_ranks = params[:project_rank]
-
+    parsed_resume = params[:parsed_resume].to_s
+    
     config = Config.first
     @min_number = config.min_number
     @max_number = config.max_number
@@ -178,18 +192,122 @@ class StudentformController < ApplicationController
         pref_score = (1.0 - (pref.to_i-1)*pref_decrease) * 100
         
         #caluclate resume score (call helper method to compare student resume to project description here)
-        resume_score = 100
+        # classify resume
+        # need to change to upload_resume(project_description, parsed_resume)
+        classification = upload_resume(course_id, parsed_resume)
+        @most_similar_job_description = classification[0]
+        @similarity_score = classification[1]
+        # flash[:most_similar_job_description] = @most_similar_job_description
+        # flash[:similarity_score] = @similarity_score
+        resume_score = @similarity_score * 100
+        puts "RESUME SCORE RESUME SCORE RESUME SCORE = " + resume_score.to_s
 
-        #add each score to DB under the scores_entity that it belongs to (student project pairing)
+        #add each score to DB under the scores_entity that it belongs to (student project pairing) THIS ADDS NEW SCORES. WHAT IF WE NEED TO REPLACE SCORES
         ScoresValue.create(scores_id: scores_id, attribute_id: time_stamp_attribute_id, feature_score: rounded_time_score)
         ScoresValue.create(scores_id: scores_id, attribute_id: pref_attribute_id, feature_score: pref_score)
         ScoresValue.create(scores_id: scores_id, attribute_id: resume_attribute_id, feature_score: resume_score)
       end
       flash[:success] = "Registration Successful!"
+      flash[:most_similar_job_description] = 'testing flash'
+      flash[:similarity_score] = 100
     else
       flash[:error] = "Failed to save student information."
     end
 
     redirect_to studentform_path
+  end
+
+  def upload_resume(course_id_, resume_)
+    
+    course_id = course_id_.to_i
+    puts "course_id: " + course_id.to_s
+    resume = resume_.to_s
+    puts "resume: " + resume 
+    
+    @descriptions = Project.where(course_id: course_id).pluck(:description)
+    puts "course description: " + @descriptions.join(separator = ",")
+    @resume_text = resume
+
+    # You can print the extracted text or use it for further processing
+    # puts "Extracted Resume Text:"
+    # puts @resume_text
+    # flash[:resume_text] = @resume_text
+    classification = classify(@resume_text, @descriptions)
+    
+    return classification
+  end
+  
+  private
+  
+  # Function to remove stop words from a string
+  def remove_stop_words(input_string, stop_words)
+    words = input_string.split
+    filtered_words = words.reject { |word| stop_words.include?(word.downcase) }
+    filtered_string = filtered_words.join(' ')
+    return filtered_string
+  end
+
+  
+  def classify(resume_text, descriptions)
+    
+    stop_words = [
+      "a", "about", "above", "after", "all", "also", "am", "an", "and", "any",
+      "are", "as", "at", "be", "because", "been", "before", "being", "between",
+      "both", "but", "by", "can", "did", "do", "each", "for", "from", "has",
+      "have", "he", "her", "here", "him", "his", "how", "i", "if", "in", "is",
+      "it", "its", "just", "like", "me", "my", "of", "on", "or", "our", "she",
+      "so", "some", "that", "the", "their", "them", "then", "there", "these",
+      "they", "this", "to", "was", "we", "were", "what", "when", "where",
+      "which", "who", "will", "with", "you", "your"
+    ]
+    
+    corpus = []
+    
+    if descriptions.nil? || descriptions.empty?
+      puts 'no job descriptions detected for course_id'
+      return ['no job descriptions detected for course_id', 0]
+    else
+      job_descriptions = descriptions
+    end
+
+    
+    job_descriptions.length.times do |i|
+      cleaned = job_descriptions[i].downcase.gsub(/[^a-z\s]/, '')
+      simplified = remove_stop_words(job_descriptions[i], stop_words)
+      corpus[i] = TfIdfSimilarity::Document.new(simplified)
+    end
+    
+    resumeidx = corpus.length # last index of corpus
+    
+    # Process the resume and calculate TF-IDF scores
+    resume = resume_text
+    corpus[resumeidx] = TfIdfSimilarity::Document.new(remove_stop_words(resume.downcase.gsub(/[^a-z\s]/, ''), stop_words))
+
+    model = TfIdfSimilarity::TfIdfModel.new(corpus)
+    
+    matrix = model.similarity_matrix
+    
+    similarity_scores = []
+    
+    # Calculate the cosine similarity between the resume and job descriptions
+    (corpus.length - 1).times do |i|
+      similarity_scores[i] = matrix[model.document_index(corpus[resumeidx]), model.document_index(corpus[i])]
+    end 
+    
+    # Set a threshold for similarity to classify as a match
+    threshold = 0.0
+    
+    # Find the best match
+    best_match_index = similarity_scores.each_with_index.max[1]
+    
+    # Check if the best match is above the threshold
+    if similarity_scores[best_match_index] > threshold
+      puts "The resume is a match for the job description: #{job_descriptions[best_match_index]}"
+      return[ job_descriptions[best_match_index], similarity_scores[best_match_index] ]
+    else
+      puts "No suitable job description found for the resume."
+      return ["No suitable job description found for the resume.", similarity_scores[best_match_index]]
+    end
+    return "done"
   end
 end
