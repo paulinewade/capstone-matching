a1aa5eb33a7b10a2bb7e4ac034d0967a88375c22
hunterzacha <96556654+hunterzacha@users.noreply.github.com>
2023-11-19 23:00:09 -0600

    improved coverage on projects + helpers (#173)

--- spec/controllers/sponsor_restrictions_controller_spec.rb
+++ spec/controllers/sponsor_restrictions_controller_spec.rb
@@ -1,59 +1,60 @@
-# spec/controllers/sponsor_restrictions_controller_spec.rb
-
 require 'rails_helper'
 
 RSpec.describe SponsorRestrictionsController, type: :controller do
-  let(:valid_params) { { project_id: 1, sponsor_restriction: { restriction_type: 'gender', restriction_val: 'Male' } } }
-  let(:invalid_params) { { project_id: 1, sponsor_restriction: { restriction_type: '', restriction_val: '' } } }
+  let(:project) { create(:project) }
+  let(:sponsor_restriction) { create(:sponsor_restriction, project: project) }
+
+  describe 'POST #create' do
+    it 'creates a new sponsor restriction' do
+      post :create, params: { project_id: project.id, sponsor_restriction: attributes_for(:sponsor_restriction) }
+
+      expect(response).to redirect_to(edit_project_path(project))
+      expect(flash[:notice]).to eq 'Sponsor Restriction was successfully created'
+    end
+
+    it 'handles invalid parameters' do
+      post :create, params: { project_id: project.id, sponsor_restriction: { invalid_param: 'value' } }
+
+      expect(response).to redirect_to(new_project_sponsor_restriction_path(project))
+      expect(flash[:error]).to_not be_nil
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'renders the edit template' do
+      get :edit, params: { project_id: project.id, id: sponsor_restriction.id }
+
+      expect(response).to render_template(:edit)
+      expect(assigns(:sponsor_restriction)).to eq(sponsor_restriction)
+    end
+  end
 
   describe 'GET #new' do
-    let!(:project1) do
-      Project.create(project_id: '1',
-                     name: 'Example Project 10',
-                     description: 'Example Description 10',
-                     sponsor: 'Example Sponsor 10',
-                     info_url: 'www.tamu.edu',
-                     semester: 'Fall 2023')
-    end
-    it 'assigns a new sponsor restriction as @sponsor_restriction' do
-      get :new, params: { project_id: 1 }
+    it 'renders the new template' do
+      get :new, params: { project_id: project.id }
+
+      expect(response).to render_template(:new)
       expect(assigns(:sponsor_restriction)).to be_a_new(SponsorRestriction)
     end
   end
 
-  describe 'POST #create' do
-    let!(:project1) do
-      Project.create(project_id: '1',
-                     name: 'Example Project 10',
-                     description: 'Example Description 10',
-                     sponsor: 'Example Sponsor 10',
-                     info_url: 'www.tamu.edu',
-                     semester: 'Fall 2023')
+  describe 'PATCH #update' do
+    let(:other_project) {create(:project)}
+    it 'updates the sponsor restriction' do
+      patch :update, params: { project_id: project.id, id: sponsor_restriction.id, sponsor_restriction: { updated_param: 'value' } }
+
+      expect(response).to redirect_to(edit_project_path(project))
+      expect(flash[:notice]).to eq 'Sponsor Restriction was successfully updated'
     end
-    context 'with valid parameters' do
-      it 'creates a new sponsor restriction' do
-        expect {
-          post :create, params: valid_params
-        }.to change(SponsorRestriction, :count).by(1)
-      end
+  end
 
-      it 'redirects to the edit project path' do
-        post :create, params: valid_params
-        expect(response).to redirect_to(edit_project_path(assigns(:project)))
-      end
-    end
+  describe 'DELETE #destroy' do
+    it 'deletes the sponsor restriction' do
+      delete :destroy, params: { project_id: project.id, id: sponsor_restriction.id }
 
-    context 'with invalid parameters' do
-      it 'does not save the new sponsor restriction' do
-        expect {
-          post :create, params: invalid_params
-        }.to_not change(SponsorRestriction, :count)
-      end
-
-      it 're-renders the new method' do
-        post :create, params: invalid_params
-        expect(response).to redirect_to(new_project_sponsor_restriction_path(assigns(:project)))
-      end
+      expect(response).to redirect_to(edit_project_path(project))
+      expect(flash[:notice]).to eq 'Sponsor Restriction was successfully deleted'
+      expect(SponsorRestriction.exists?(sponsor_restriction.id)).to be_falsey
     end
   end
 end
