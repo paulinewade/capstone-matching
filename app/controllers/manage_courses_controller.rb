00052e3feca75b8082680db0565af6382df99b66
Shripad Kulkarni <shripad1020@gmail.com>
2023-11-20 01:19:19 -0600

    Form to add new course from manage courses page

--- app/controllers/manage_courses_controller.rb
+++ app/controllers/manage_courses_controller.rb
@@ -2,10 +2,9 @@ class ManageCoursesController < ApplicationController
 	def index
 		@course = Course.pluck(:Semester).uniq
 		@selected_semester = params[:semester]
+		@semesters = generate_semesters
 
-		# @professors = User.includes(:professor => :courses).where.not(professors: { professor_id: nil })
 		@professors_for_select = Professor.all.map { |professor| ["#{professor.user.first_name.capitalize} #{professor.user.last_name.capitalize}", professor.id] }
-
 
 		@courses = if @selected_semester.present?
 			Course.where(semester: @selected_semester)
@@ -15,7 +14,6 @@ class ManageCoursesController < ApplicationController
 	end
 
 	def edit_courses
-
 		update_professor_assignments if params[:course_assignments].present?
 		delete_courses if params[:delete_course].present?
 
@@ -24,8 +22,23 @@ class ManageCoursesController < ApplicationController
 
 	end
 
-	private
+	def add_course
+		@course_number = params[:course_number]
+		@section = params[:section]
+		@semester = params[:semester]
+		@professor = params[:professor_id]
 
+		@new_course = Course.new(course_number: @course_number, section: @section, semester: @semester, professor_id: @professor)
+
+		if @new_course.save
+			flash[:success] = "Course added successfully."
+			redirect_to manageCourses_path
+		else
+			render :index
+		end
+	end
+
+	private
 		def update_professor_assignments
 			params[:course_assignments].each do |course_id, professor_id|
 				course = Course.find(course_id)
@@ -37,4 +50,17 @@ class ManageCoursesController < ApplicationController
 			Course.where(course_id: params[:delete_course]).destroy_all
 		end
 
+		def generate_semesters
+			current_year = Time.now.year
+			semesters = []
+
+			(0..1).each do |year_offset|
+				year = current_year + year_offset
+				semesters << "Spring #{year}"
+				semesters << "Summer #{year}"
+				semesters << "Fall #{year}"
+			end
+
+			semesters
+		end
 end
