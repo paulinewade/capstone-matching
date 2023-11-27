3f93a04229de52cec3caf7eae34c0c906ef8e6d0
Hunter Zacha <hunterzacha@tamu.edu>
2023-11-27 10:30:07 -0600

    added name display on landing pages

--- app/controllers/adminlanding_controller.rb
+++ app/controllers/adminlanding_controller.rb
@@ -1,6 +1,9 @@
 class AdminlandingController < ApplicationController
     before_action :authorize_admin, unless: -> { Rails.env.development? || Rails.env.test? }
     def index
+        @current_user = User.find_by(id: session[:user_id])
+        @first_name = @current_user.first_name
+        @last_name = @current_user.last_name
     end
 
     def configuration
--- app/controllers/prof_landing_controller.rb
+++ app/controllers/prof_landing_controller.rb
@@ -1,5 +1,8 @@
 class ProfLandingController < ApplicationController
     before_action :authorize_admin_or_prof, unless: -> { Rails.env.development? || Rails.env.test? }
     def index
+        @current_user = User.find_by(id: session[:user_id])
+        @first_name = @current_user.first_name
+        @last_name = @current_user.last_name
     end
 end
