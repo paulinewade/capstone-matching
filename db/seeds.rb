1961656b543252fbe8f019f83b455643868dc2a2
Hunter_Zacha <hunterz7890@gmail.com>
2023-12-05 15:01:26 -0600

    added prof col to results, made csv export match

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,6 +36,7 @@ config = Config.create(min_number: 2, max_number: 10, form_open: form_open_datet
 student_user = User.create(user_id: 101, first_name: "student", last_name: "example", role: "student", email: "studentemail@tamu.edu")
 prof_user = User.create(first_name: "professor", last_name: "example", role: "professor", email: "profemail@tamu.edu")
 admin_user  = User.create(first_name: "admin", last_name: "example", role: "admin", email: "adminemail@tamu.edu")
+student_user2 = User.create(first_name: "student", last_name: "example2", role: "student2", email: "studentemail2@tamu.edu")
 
 prof = Professor.create(professor_id: prof_user.user_id, verified: true, admin: false)
 admin = Professor.create(professor_id: admin_user.user_id, verified: true, admin: true)
@@ -66,17 +67,24 @@ student = Student.create(student_id: student_user.user_id, course_id: course.cou
 ethnicity_value1 = EthnicityValue.create(student_id: student.student_id, ethnicity_name: "White")
 ethnicity_value2 = EthnicityValue.create(student_id: student.student_id, ethnicity_name: "Black or African American")
 
+student2 = Student.create(student_id: student_user2.user_id, course_id: course3.course_id, gender: "Male", nationality: "American", work_auth: "US Citizen", contract_sign: "Ok with Any Agreements", uin: 123123124, resume: "Nodejs, Javascript, Java, Python")
+ethnicity_value3 = EthnicityValue.create(student_id: student2.student_id, ethnicity_name: "White")
+ethnicity_value4 = EthnicityValue.create(student_id: student2.student_id, ethnicity_name: "Black or African American")
+
 project = Project.create(name: "Example Project00", description: "Example Description", sponsor: "Example Sponsor", course_id: course.course_id, info_url: "https://www.tamu.edu", semester: "Fall 2023")
 project01 = Project.create(name: "Example Project01", description: "This is a test descriptor", sponsor: "Example Sponsor", course_id: course.course_id, info_url: "https://www.tamu.edu", semester: "Fall 2023")
 project2 = Project.create(name: "Example Project2", description: "Example Description2", sponsor: "Example Sponsor2", semester: "Spring 2024")
 project3 = Project.create(name: "Example Project3", description: "Example Description3", sponsor: "Example Sponsor3", semester: "Fall 2023")
 
 attribute1 = ScoresAttribute.create(feature: "Resume/Skills Match Score", feature_weight: 0.5)
-attribute2 = ScoresAttribute.create(feature: "Preference Match Score", feature_weight: 0.25)
+attribute2 = ScoresAttribute.create(feature: "Student Preference Match Score", feature_weight: 0.25)
 attribute3 = ScoresAttribute.create(feature: "Time Submitted Score", feature_weight: 0.25)
 
 scores_entity = ScoresEntity.create(student_id: student.student_id, project_id: project.project_id, pref: 1)
 scores_entity2 = ScoresEntity.create(student_id: student.student_id, project_id: project01.project_id, pref: 2)
+
+scores_entity3 = ScoresEntity.create(student_id: student2.student_id, project_id: project.project_id, pref: 1)
+scores_entity4 = ScoresEntity.create(student_id: student2.student_id, project_id: project01.project_id, pref: 2)
 
 
 scores_value1 = ScoresValue.create(scores_id: scores_entity.scores_id, attribute_id: attribute1.attribute_id, feature_score: 50)
@@ -87,9 +95,18 @@ scores_value1_2 = ScoresValue.create(scores_id: scores_entity2.scores_id, attrib
 scores_value2_2 = ScoresValue.create(scores_id: scores_entity2.scores_id, attribute_id: attribute2.attribute_id, feature_score: 100)
 scores_value3_2 = ScoresValue.create(scores_id: scores_entity2.scores_id, attribute_id: attribute3.attribute_id, feature_score: 100)
 
+scores_value1 = ScoresValue.create(scores_id: scores_entity4.scores_id, attribute_id: attribute1.attribute_id, feature_score: 50)
+scores_value2 = ScoresValue.create(scores_id: scores_entity4.scores_id, attribute_id: attribute2.attribute_id, feature_score: 100)
+scores_value3 = ScoresValue.create(scores_id: scores_entity4.scores_id, attribute_id: attribute3.attribute_id, feature_score: 75)
+
+scores_value1_2 = ScoresValue.create(scores_id: scores_entity3.scores_id, attribute_id: attribute1.attribute_id, feature_score: 100)
+scores_value2_2 = ScoresValue.create(scores_id: scores_entity3.scores_id, attribute_id: attribute2.attribute_id, feature_score: 100)
+scores_value3_2 = ScoresValue.create(scores_id: scores_entity3.scores_id, attribute_id: attribute3.attribute_id, feature_score: 100)
+
 professor_pref = ProfessorPreference.create(project_id: project.project_id, professor_id: prof.professor_id, pref: 1)
 
 restriction = SponsorRestriction.create(restriction_type: "gender", restriction_val: "Male", project_id: project.project_id)
+restriction2 = SponsorRestriction.create(restriction_type: "gender", restriction_val: "Female", project_id: project.project_id)
 preference = SponsorPreference.create(project_id: project.project_id, preference_type: "gender", preference_val: "Non-Binary", bonus_amount: 10)
 
 # project3 = Project.create(name: "Example Project 3", description: "Example Description 3", sponsor: "Example Sponsor 3", course_id: course3.course_id, info_url: "www.tamu.edu", semester: "Fall 2023")
