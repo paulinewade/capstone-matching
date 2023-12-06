8cabe7c924bda96d9de9378a55b010c042b016b3
Hunter Zacha <hunterzacha@tamu.edu>
2023-12-06 14:46:46 -0600

    default seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,25 +10,6 @@
 profwade = User.create(email: 'paulinewade@tamu.edu', first_name: 'Pauline', last_name: 'Wade', role: 'admin')
 adminwade = Professor.create(professor_id: profwade.user_id, verified: true, admin: true)
 
-hunter = User.create(email: 'hunterzacha@tamu.edu', first_name: 'Hunter', last_name: 'Zacha', role: 'admin')
-adminhunter = Professor.create(professor_id: hunter.user_id, verified: true, admin: true)
-
-aakash = User.create(email: 'asil@tamu.edu', first_name: 'Aakashdeep', last_name: 'Sil', role: 'admin')
-adminaakash = Professor.create(professor_id: aakash.user_id, verified: true, admin: true)
-
-yash = User.create(email: 'yapatil@tamu.edu', first_name: 'Yash', last_name: 'Patil', role: 'admin')
-adminyash = Professor.create(professor_id: yash.user_id, verified: true, admin: true)
-
-shripad = User.create(email: 'shripad@tamu.edu', first_name: 'Shripad', last_name: 'Kulkarni', role: 'admin')
-adminshripad = Professor.create(professor_id: shripad.user_id, verified: true, admin: true)
-
-zach = User.create(email: 'zacharylaguna@tamu.edu', first_name: 'Zachary', last_name: 'Laguna', role: 'admin' )
-adminzach = Professor.create(professor_id: zach.user_id, verified: true, admin: true)
-
-sathvik = User.create(email: 'sathvikkote@tamu.edu', first_name: 'Sathvik', last_name: 'Kote', role: 'admin')
-adminsathvik = Professor.create(professor_id: sathvik.user_id, verified: true, admin: true)
-
-
 form_open_datetime = DateTime.new(2023, 11, 6, 9, 0) # Example date and time
 form_close_datetime = DateTime.new(2023, 12, 6, 17, 0) # Example date and time
 fall_sem_month = 8
@@ -40,21 +21,6 @@ summer_sem_day = 1
 
 config = Config.create(min_number: 2, max_number: 10, form_open: form_open_datetime , form_close:form_close_datetime, fall_semester_month: fall_sem_month , fall_semester_day: fall_sem_day, spring_semester_month: spring_sem_month , 
 spring_semester_day: spring_sem_day, summer_semester_month: summer_sem_month, summer_semester_day: summer_sem_day)
-
-student_user = User.create(user_id: 101, first_name: "student", last_name: "example", role: "student", email: "studentemail@tamu.edu")
-prof_user = User.create(first_name: "professor", last_name: "example", role: "professor", email: "profemail@tamu.edu")
-admin_user  = User.create(first_name: "admin", last_name: "example", role: "admin", email: "adminemail@tamu.edu")
-student_user2 = User.create(first_name: "student", last_name: "example2", role: "student2", email: "studentemail2@tamu.edu")
-
-prof = Professor.create(professor_id: prof_user.user_id, verified: true, admin: false)
-admin = Professor.create(professor_id: admin_user.user_id, verified: true, admin: true)
-
-course = Course.create(course_number: 606, section: 600, semester: "Fall 2023", professor_id: prof.professor_id)
-course2 = Course.create(course_number: 606, section: 601, semester: "Fall 2023", professor_id: admin.professor_id)
-course3 = Course.create(course_number: 606, section: 602, semester: "Fall 2023")
-course4 = Course.create(course_number: 606, section: 602, semester: "Spring 2024")
-course5 = Course.create(course_id: 101, course_number: 606, section: 603, semester: "Fall 2023")
-
 
 ethnicities = [
   "White",
@@ -70,56 +36,8 @@ ethnicities.each do |ethnicity|
   Ethnicity.find_or_create_by(ethnicity_name: ethnicity)
 end
 
-
-student = Student.create(student_id: student_user.user_id, course_id: course.course_id, gender: "Male", nationality: "American", work_auth: "US Citizen", contract_sign: "Ok with Any Agreements", uin: 123123123, resume: "Nodejs, Javascript, Java, Python")
-ethnicity_value1 = EthnicityValue.create(student_id: student.student_id, ethnicity_name: "White")
-ethnicity_value2 = EthnicityValue.create(student_id: student.student_id, ethnicity_name: "Black or African American")
-
-student2 = Student.create(student_id: student_user2.user_id, course_id: course3.course_id, gender: "Male", nationality: "American", work_auth: "US Citizen", contract_sign: "Ok with Any Agreements", uin: 123123124, resume: "Nodejs, Javascript, Java, Python")
-ethnicity_value3 = EthnicityValue.create(student_id: student2.student_id, ethnicity_name: "White")
-ethnicity_value4 = EthnicityValue.create(student_id: student2.student_id, ethnicity_name: "Black or African American")
-
-project = Project.create(name: "Example Project00", description: "Example Description", sponsor: "Example Sponsor", course_id: course.course_id, info_url: "https://www.tamu.edu", semester: "Fall 2023")
-project01 = Project.create(name: "Example Project01", description: "This is a test descriptor", sponsor: "Example Sponsor", course_id: course.course_id, info_url: "https://www.tamu.edu", semester: "Fall 2023")
-project2 = Project.create(name: "Example Project2", description: "Example Description2", sponsor: "Example Sponsor2", semester: "Spring 2024")
-project3 = Project.create(name: "Example Project3", description: "Example Description3", sponsor: "Example Sponsor3", semester: "Fall 2023")
-
 attribute1 = ScoresAttribute.create(feature: "Resume/Skills Match Score", feature_weight: 0.5)
 attribute2 = ScoresAttribute.create(feature: "Student Preference Match Score", feature_weight: 0.25)
 attribute3 = ScoresAttribute.create(feature: "Time Submitted Score", feature_weight: 0.25)
 
-scores_entity = ScoresEntity.create(student_id: student.student_id, project_id: project.project_id, pref: 1)
-scores_entity2 = ScoresEntity.create(student_id: student.student_id, project_id: project01.project_id, pref: 2)
-
-scores_entity3 = ScoresEntity.create(student_id: student2.student_id, project_id: project.project_id, pref: 1)
-scores_entity4 = ScoresEntity.create(student_id: student2.student_id, project_id: project01.project_id, pref: 2)
-
-
-scores_value1 = ScoresValue.create(scores_id: scores_entity.scores_id, attribute_id: attribute1.attribute_id, feature_score: 50)
-scores_value2 = ScoresValue.create(scores_id: scores_entity.scores_id, attribute_id: attribute2.attribute_id, feature_score: 100)
-scores_value3 = ScoresValue.create(scores_id: scores_entity.scores_id, attribute_id: attribute3.attribute_id, feature_score: 75)
-
-scores_value1_2 = ScoresValue.create(scores_id: scores_entity2.scores_id, attribute_id: attribute1.attribute_id, feature_score: 100)
-scores_value2_2 = ScoresValue.create(scores_id: scores_entity2.scores_id, attribute_id: attribute2.attribute_id, feature_score: 100)
-scores_value3_2 = ScoresValue.create(scores_id: scores_entity2.scores_id, attribute_id: attribute3.attribute_id, feature_score: 100)
-
-scores_value1 = ScoresValue.create(scores_id: scores_entity4.scores_id, attribute_id: attribute1.attribute_id, feature_score: 50)
-scores_value2 = ScoresValue.create(scores_id: scores_entity4.scores_id, attribute_id: attribute2.attribute_id, feature_score: 100)
-scores_value3 = ScoresValue.create(scores_id: scores_entity4.scores_id, attribute_id: attribute3.attribute_id, feature_score: 75)
-
-scores_value1_2 = ScoresValue.create(scores_id: scores_entity3.scores_id, attribute_id: attribute1.attribute_id, feature_score: 100)
-scores_value2_2 = ScoresValue.create(scores_id: scores_entity3.scores_id, attribute_id: attribute2.attribute_id, feature_score: 100)
-scores_value3_2 = ScoresValue.create(scores_id: scores_entity3.scores_id, attribute_id: attribute3.attribute_id, feature_score: 100)
-
-professor_pref = ProfessorPreference.create(project_id: project.project_id, professor_id: prof.professor_id, pref: 1)
-
-restriction = SponsorRestriction.create(restriction_type: "gender", restriction_val: "Male", project_id: project.project_id)
-restriction2 = SponsorRestriction.create(restriction_type: "gender", restriction_val: "Female", project_id: project.project_id)
-preference = SponsorPreference.create(project_id: project.project_id, preference_type: "gender", preference_val: "Non-Binary", bonus_amount: 10)
-
-# project3 = Project.create(name: "Example Project 3", description: "Example Description 3", sponsor: "Example Sponsor 3", course_id: course3.course_id, info_url: "www.tamu.edu", semester: "Fall 2023")
-project4 = Project.create(name: "Example Project 4", description: "We are looking for a software engineer with expertise in Ruby and Ruby on Rails.", sponsor: "Example Sponsor 4", course_id: course5.course_id, info_url: "https://www.tamu.edu", semester: "Fall 2023")
-project5 = Project.create(name: "Example Project 5", description: "Seeking a front-end developer experienced in JavaScript and React.", sponsor: "Example Sponsor 5", course_id: course5.course_id, info_url: "https://www.tamu.edu", semester: "Fall 2023")
-project6 = Project.create(name: "Example Project 6", description: "We need a data scientist skilled in Python and machine learning.", sponsor: "Example Sponsor6", course_id: course5.course_id, info_url: "https://www.tamu.edu", semester: "Fall 2023")
-project7 = Project.create(name: "Example Project 7", description: "We need a data scientist skilled in Python and machine learning.", sponsor: "Example Sponsor7", course_id: course5.course_id, semester: "Fall 2023")
 
